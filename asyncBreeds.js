81822fd858a83ede08b18e06424b59b8d2dcc944
Camilo Gomez <camilogomezmusic@gmail.com>
2020-07-28 21:03:31 +0000

    completed cats exercise

---
+++ asyncBreeds.js
@@ -0,0 +1,17 @@
+const fs = require('fs');
+
+const printData = function(data) {
+  console.log('Return value: ', data);
+}
+
+const breedDetailsFromFile = function(breed, callback) {
+  console.log('breedDetailsFromFile: Calling readFile..."');
+  fs.readFile(`./data/${breed}.txt`, 'utf8', (error, data) => {
+    console.log("In readFile's Callback: it has the data.");
+
+    if (!error) callback(data);
+  });
+};
+
+
+breedDetailsFromFile('Bombay', printData);
